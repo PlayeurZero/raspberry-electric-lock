96d235c2d0fdf98cbae4466a547b919035441761
Nicolas MATHIEU <n.mathieu@splitfire.fr>
2025-02-10 20:55:33 +0100

    dev

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,26 @@
-const CACHE_NAME = 'raspberry-electric-lock:1.0.0-alpha.0';
+const CACHE_NAME = "raspberry-electric-lock:1.0.0-alpha.0";
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/manifest.json',
-        'https://cdn.tailwindcss.com'
-      ]))
+    caches
+      .open(CACHE_NAME)
+      .then((cache) =>
+        cache.addAll([
+          "/",
+          "/index.html",
+          "/style.css",
+          "/script.js",
+          "/manifest.json",
+          "https://cdn.tailwindcss.com",
+        ]),
+      ),
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request)),
   );
 });

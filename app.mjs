2b9c3e0aa863736c6b066755bfc98f793f45231c
Nicolas Mathieu <n.mathieu@splitfire.fr>
2025-02-12 17:58:44 +0100

    feat: pwa support

--- app.mjs
+++ app.mjs
@@ -1,8 +1,10 @@
 // deps
 import "dotenv/config";
 import http from "node:http";
+import https from "node:https";
 import rpio from "rpio";
 import fs from "node:fs/promises";
+import { readFileSync } from "node:fs";
 import { createReadStream } from "node:fs";
 import path from "node:path";
 
@@ -51,6 +53,7 @@ const raspberry = new (class {
   }
 
   async openRelay(source) {
+    return;
     if (rpio.LOW === rpio.read(this.#relay)) {
       await this.#log(`(${source}) relay open request`);
 
@@ -67,20 +70,63 @@ const raspberry = new (class {
   }
 
   runHttpServer() {
-    const server = http.createServer(async function (req, res) {
-      if ("/" === req.url) {
-        createReadStream("./app.html").pipe(res);
-      } else if ("/unlock" === req.url) {
-        await raspberry.openRelay("http");
-        res.writeHead(204);
-        res.end();
-      } else {
-        res.writeHead(404);
-        res.end();
-      }
+    const httpsServer = https.createServer(
+      {
+        key: readFileSync(process.env.HTTPS_KEY_FILEPATH),
+        cert: readFileSync(process.env.HTTPS_CERT_FILEPATH),
+      },
+      async function (req, res) {
+        if (req.url.includes("..")) {
+          res.writeHead(400);
+          res.end();
+        }
+
+        if ("/api/unlock" === req.url) {
+          raspberry.openRelay("http");
+          res.writeHead(204);
+          res.end();
+        } else {
+          let filepath = path.resolve(
+            "public",
+            "/" === req.url ? "index.html" : req.url.slice(1),
+          );
+
+          try {
+            await fs.stat(path.resolve(filepath));
+            let fileExt = path.extname(filepath);
+
+            if (fileExt === ".html") {
+              res.setHeader("content-type", "text/html");
+            } else if (fileExt === ".js") {
+              res.setHeader("content-type", "text/javascript");
+            } else if (fileExt === ".png") {
+              //            res.setHeader("content-type", "image/png")
+            } else if (fileExt === ".json") {
+              res.setHeader("content-type", "application/json");
+            }
+
+            createReadStream(filepath).pipe(res);
+          } catch {
+            res.writeHead(404);
+            res.end();
+          }
+        }
+      },
+    );
+
+    httpsServer.listen(process.env.HTTPS_SERVER_PORT);
+
+    const httpServer = http.createServer(async function (req, res) {
+      const hostname = req.headers.host.split(":")[0];
+      res.setHeader(
+        "location",
+        `https://${hostname}:${process.env.HTTPS_SERVER_PORT}`,
+      );
+      res.writeHead(301);
+      res.end();
     });
 
-    server.listen(8080);
+    httpServer.listen(process.env.HTTP_SERVER_PORT);
   }
 })();
 
@@ -93,7 +139,6 @@ main();
 
 function handleExit(error) {
   console.error(error);
-
   rpio.exit();
   process.exit();
 }
